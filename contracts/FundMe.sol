e09159dea86c7e62b36abb3db6b4de506fc8d63a
xyizko <xyizko@proton.me>
2025-03-08 22:38:15 +0530

    - test funding contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -11,12 +11,19 @@ pragma solidity ^0.8.18;
 
 contract FundMe {
 
+    // Success Message event
+    event Funded(address indexed sender, uint256 amount, string message);
+
     // Function that actuall used for funding the contract which will be deployed
-    function fund() public {
+    function fund() public payable {
 
         // Allow users to send $ 
 
-        // Have a minimum $ set 
+        // Have a minimum $ set - Here its 100Wei
+        require (msg.value >= 100, "Not Enough"); // 1ETH = 1e18 
+
+        // Success Message event
+        emit Funded(msg.sender, msg.value, "Funding Successful");
 
         // 1. Determing how to sent funds to this contract
 
