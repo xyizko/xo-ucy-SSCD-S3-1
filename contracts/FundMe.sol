b4d4a75213086ee1d53dd1981c16b3434d67d511
xyizko <xyizko@proton.me>
2025-03-09 00:13:13 +0530

    - add Notes

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,38 +1,20 @@
-// Planning section of the contract 
-// Get funds from users 
-// Withdraw Funds
-// Set minimum funding value in usd 
+// Check the FundMe.Sol inside others/ for tests
 
-// SPDX-License-Identifier: MITZ
+//SPDX-License-Identifier: Bananana 
 
 pragma solidity ^0.8.18;
 
-// Next step in planning is first write out all the required functions in the contract 
-
 contract FundMe {
 
-    // uint256 public myValue = 1;
+    uint256 minimumUsd = 1.0;
 
-    // Success Message event - Message in the raw logs
-    event Funded(address indexed sender, uint256 amount, string message);
+    function fund() public payable{
+    
+    // Allow users to send $
+    // Have minimum 0.50$ sent
+    // 1. How to send eth to this contract 
 
-    // Function that actuall used for funding the contract which will be deployed
-    function fund() public payable {
-
-        // Allow users to send $ 
-
-        // Have a minimum $ set - Here its 100Wei- Require make the amount mandatory
-        // myValue = myValue + 2;
-        require (msg.value >= 100, "Not Enough"); // 1ETH = 1e18 
-
-        // Success Message event
-        emit Funded(msg.sender, msg.value, "Funding Successful");
-
-        // 1. Determing how to sent funds to this contract
-
-
-
-    } 
-
-    // function withdraw() public {}
+    require(msg.value > 100, "Minimum 100 Banans"); // 1e18 = 1 ETH = 1,000,000,000,000,000,000
+    
+    }
 }
