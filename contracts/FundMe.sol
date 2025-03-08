9c1d0af09b4e6ba1ed74668157ba9c2d33867bca
xyizko <xyizko@proton.me>
2025-03-08 21:42:13 +0530

    - Notez

---
+++ contracts/FundMe.sol
@@ -0,0 +1,28 @@
+// Planning section of the contract 
+// Get funds from users 
+// Withdraw Funds
+// Set minimum funding value in usd 
+
+// SPDX-License-Identifier: MITZ
+
+pragma solidity ^0.8.18;
+
+// Next step in planning is first write out all the required functions in the contract 
+
+contract FundMe {
+
+    // Function that actuall used for funding the contract which will be deployed
+    function fund() public {
+
+        // Allow users to send $ 
+
+        // Have a minimum $ set 
+
+        // 1. Determing how to sent funds to this contract
+
+
+
+    } 
+
+    // function withdraw() public {}
+}
